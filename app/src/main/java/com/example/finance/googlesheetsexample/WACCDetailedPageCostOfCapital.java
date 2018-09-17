b4a4267749eafe464662030d7d6783eb1d067f0c
adam0000345 <adam000034@hotmail.com>
2018-09-16 21:46:38 -0700

    Get consistent menus set up on every page, get WACCDetailed done, get home screen done. Get Cost of Capital done and get ability to read in data from a .xls or .xlsx file specifically.  Eventually, using Retrofit or another tool to download files from the following server would be helpful as well: http://people.stern.nyu.edu/adamodar/pc/datasets/.

--- app/src/main/java/com/example/finance/googlesheetsexample/WACCDetailedPageCostOfCapital.java
+++ app/src/main/java/com/example/finance/googlesheetsexample/WACCDetailedPageCostOfCapital.java
@@ -5,7 +5,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
+//test
 import com.facebook.drawee.backends.pipeline.Fresco;
 
 public class WACCDetailedPageCostOfCapital extends AppCompatActivity {
